07cbf85daf04e067d30dc677c2cbd3dfb72cef81
Andrew Larson <andrew.p.larson@gmail.com>
2014-11-25 10:23:54 -0800

    Project Config

---
+++ app/assets/javascripts/aero_bnb.js
@@ -0,0 +1,13 @@
+window.AeroBnb = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  AeroBnb.initialize();
+});
