70f1cc24ec764e12c17eaa770aa4c85be8c2ec08
Andrew Larson <andrew.p.larson@gmail.com>
2014-11-26 09:30:24 -0800

    Make airport select view

--- app/assets/javascripts/views/airports/airport_select.js
+++ app/assets/javascripts/views/airports/airport_select.js
@@ -1,3 +1,13 @@
 AeroBnb.Views.AirportSelect = Backbone.View.extend({
-  
+  template: JST["airports/select"],
+
+  initialize: function (options) {
+    this.airports = options.airports;
+  },
+
+  render: function () {
+    var content = this.template({ airports: this.airports });
+    this.$el.html(content);
+    return this;
+  }
 })
