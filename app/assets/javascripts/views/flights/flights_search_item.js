aa89d3ebbf415b0faeb98412054458f4cf599cfe
Andrew Larson <andrew.p.larson@gmail.com>
2014-11-25 17:24:55 -0800

    Backbone navigation to flight show pages

--- app/assets/javascripts/views/flights/flights_search_item.js
+++ app/assets/javascripts/views/flights/flights_search_item.js
@@ -3,13 +3,20 @@ AeroBnb.Views.FlightsSearchItem = Backbone.View.extend({
 
   initialize: function (options) {
     this.flight = options.flight;
-    this.$el.addClass('search-item');
     this.listenTo(this.flight, 'sync', this.render)
+  },
+
+  events: {
+    'click .search-item': 'showFlight'
   },
 
   render: function () {
     var content = this.template({ flight: this.flight });
     this.$el.html(content);
     return this;
+  },
+
+  showFlight: function (event) {
+    Backbone.history.navigate("flights/" + this.flight.id, { trigger: true })
   }
 })
