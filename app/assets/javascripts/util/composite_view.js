f6e7473790999d898992b34a3f20e5ea53c41e23
Andrew Larson <andrew.p.larson@gmail.com>
2014-11-26 09:05:52 -0800

    Begin writing composite view

---
+++ app/assets/javascripts/util/composite_view.js
@@ -0,0 +1,12 @@
+Backbone.CompositeView = Backbone.View.extend({
+  subviews: function (selector) {
+    this._subviews = this._subviews || {};
+
+    if (!selector) {
+      return this._subviews;
+    } else {
+      this._subviews[selector] = this._subviews[selector] || [];
+      return this._subviews[selector];
+    }
+  }
+})
