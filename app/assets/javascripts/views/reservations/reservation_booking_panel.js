193ff5032f532e2c9e0ba576f7b72c3ba24946e8
Andrew Larson <andrew.p.larson@gmail.com>
2014-11-28 10:29:30 -0800

    Booking form on page

--- app/assets/javascripts/views/reservations/reservation_booking_panel.js
+++ app/assets/javascripts/views/reservations/reservation_booking_panel.js
@@ -1,8 +1,12 @@
 AeroBnb.Views.ReservationsBooking = Backbone.View.extend({
   template: JST["reservations/booking"],
 
+  initialize: function (options) {
+    this.flight = options.flight;
+  },
+
   render: function () {
-    var content = this.template();
+    var content = this.template({ flight: this.flight });
     this.$el.html(content);
     return this;
   }
