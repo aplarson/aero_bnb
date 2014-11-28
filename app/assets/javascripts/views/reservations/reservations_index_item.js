207c3439aff413d61df3cd0bab6560bc64c36b12
Andrew Larson <andrew.p.larson@gmail.com>
2014-11-28 11:11:14 -0800

    Create reservation index item view

---
+++ app/assets/javascripts/views/reservations/reservations_index_item.js
@@ -0,0 +1,15 @@
+AeroBnb.Views.ReservationsIndexItem = Backbone.View.extend({
+  template: JST["reservations/index_item"],
+
+  intialize: function (options) {
+    this.reservation = options.reservation;
+    this.$el = $('<li>').addClass('index-item');
+    this.listenTo(this.reservation, 'sync', this.render);
+  },
+
+  render: function () {
+    var content = this.template({ reservation: this.reservation });
+    this.$el.html(content);
+    return this;
+  }
+});
