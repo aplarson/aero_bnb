545960a033fdd85adb2cd01c3379a7fb17d5516f
Andrew Larson <andrew.p.larson@gmail.com>
2014-12-03 13:46:23 -0800

    Working slider for por price filter

--- app/assets/javascripts/views/flights/flights_filter.js
+++ app/assets/javascripts/views/flights/flights_filter.js
@@ -4,16 +4,33 @@ AeroBnb.Views.FlightsFilter = Backbone.CompositeView.extend({
   initialize: function (options) {
     this.queryParams = options.queryParams;
     var airportFilter = new AeroBnb.Views.AirportsFilter({ 
-                                      airports: options.airports,
-                                      selected: this.queryParams["airport"]
-                                    });
+                          airports: options.airports,
+                          selected: this.queryParams["airport"]
+                        });
     this.addSubview('#airport-select', airportFilter);
   },
 
   render: function () {
     var content = this.template({ params: this.queryParams });
     this.$el.html(content);
+    var maxPrice = this.queryParams["max_price"] || 5000;
+    var minPrice = this.queryParams["min_price"] || 0;
+    this.$('#price-filter').slider({
+      range: true,
+      min: 0,
+      max: 5000,
+      values: [minPrice, maxPrice],
+      slide: this.updatePrice.bind(this),
+      stop: function () {
+        $("#filter-form").trigger('change');
+      }
+    })
     this.attachSubviews();
     return this;
+  },
+
+  updatePrice: function (event, ui) {
+    this.$('#min-price').val(ui.values[0]);
+    this.$('#max-price').val(ui.values[1]);
   }
 })
