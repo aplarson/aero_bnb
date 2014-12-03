b20e4ce07a0f4662231697824bbd89d6603db385
Andrew Larson <andrew.p.larson@gmail.com>
2014-12-03 09:26:36 -0800

    Style sign up and log in, add errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       redirect_to root_url
     else
       @user = User.new(user_params)
-      flash.now[:errors] = "Invalid credentials"
+      flash.now[:errors] = ["Invalid credentials"]
       render :new
     end
   end
