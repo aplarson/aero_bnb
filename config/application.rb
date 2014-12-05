e11aa790f547ba58db2208f8651c45d5f96e49d8
Andrew Larson <andrew.p.larson@gmail.com>
2014-12-05 08:41:31 -0800

    Switch to Rack::Deflater

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module AeroBnb
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.middleware.swap(ActionDispatch::Static, Rack::Zippy::AssetServer)
+    config.middleware.use Rack::Deflater
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
